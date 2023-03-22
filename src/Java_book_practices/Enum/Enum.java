dbf2bd1e08634bbaf068086b9aab99e805b3f2a8
TugbaNurAlparslan <nurtugba2110@gmail.com>
2023-03-22 22:18:25 +0300

    added method in enum class

--- src/Java_book_practices/Enum/Enum.java
+++ src/Java_book_practices/Enum/Enum.java
@@ -2,18 +2,22 @@ package Java_book_practices.Enum;
 
 public class Enum {
     enum Cars{
-        AUDI, MERCEDES, BMW;
+        AUDI("blue", 100), MERCEDES("black", 200), BMW("red", 180);
        public String color;
        public int speed;
-/* 
-        AUDI("purple", 220), MERCEDES("red", 230), BMW("blue", 240);
-        public String color;
-        public int speed;
 
-        Cars(String color, int speed) {
+       Cars(String color, int speed) {
         this.color = color;
         this.speed=speed;
         }
+
+        int getSpeed(){
+            return speed;
+        }
+/* 
+        AUDI(), MERCEDES(), BMW();
+        public String color;
+        public int speed;
 
 */
         
@@ -21,6 +25,8 @@ public class Enum {
     }
     public static void main(String[] args) {
 
-        System.out.println(Cars.AUDI);
+        System.out.println(Cars.AUDI);//AUDI
+        System.out.println(Cars.BMW.ordinal()); //2 // ordinal gives indis value of BMW.
+        System.out.println(Cars.MERCEDES.getSpeed()); //200
 }
 }
