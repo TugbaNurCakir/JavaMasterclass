2501ef72edc251141acac609191dae8f42e18baa
TugbaNurAlparslan <nurtugba2110@gmail.com>
2023-03-22 22:07:41 +0300

    tested enums

---
+++ src/Java_book_practices/Enum/Enum.java
@@ -0,0 +1,28 @@
+package Java_book_practices.Enum;
+
+import JavaMasterClassFolders.Inheritance.Vehicle.Car;
+
+public class Enum {
+    enum Cars{
+        AUDI, MERCEDES, BMW;
+       public String color;
+       public int speed;
+/* 
+        AUDI("purple", 220), MERCEDES("red", 230), BMW("blue", 240);
+        public String color;
+        public int speed;
+
+        Cars(String color, int speed) {
+        this.color = color;
+        this.speed=speed;
+        }
+
+*/
+        
+        
+    }
+    public static void main(String[] args) {
+
+        System.out.println(Cars.AUDI);
+}
+}
